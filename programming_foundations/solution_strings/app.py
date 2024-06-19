7912887cd4a06dc4683a00c4a0ef948124c34e3e
SirElmard <31033760+SirElmard@users.noreply.github.com>
2024-06-19 00:57:19 +0000

    Programming Foundations exercises

---
+++ programming_foundations/solution_strings/app.py
@@ -0,0 +1,9 @@
+#convert miles to kilometers
+
+miles = input('Enter the number of miles')
+
+#kilometers_value = miles_value * 1.609344
+miles_float = float(miles)
+kilometers = miles_float * 1.609344
+print('That value in kilometers is')
+print(kilometers)
